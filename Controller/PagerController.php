6955081767d8e178a76744e2050e3bde70506d8e
ShoheiTai <shouhei.tai@gmail.com>
2015-06-15 20:35:39 +0900

    不要ファイルの削除

--- Controller/PagerController.php
+++
@@ -1,9 +0,0 @@
-<?php
-App::uses('AppController', 'Controller');
-class PagerController extends AppController {
-
-    public $components = array('Pager.Pager');
-
-    public $helpers = array('Pager.Pager');
-
-}
--- Model/PagerAppModel.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-App::uses('AppModel', 'Model');
-
-class PagerAppModel extends AppModel {
-
-}
